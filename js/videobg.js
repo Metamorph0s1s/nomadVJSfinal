b2fdd83447b53cb23ef331a9aea641373c204163
Metamorph0s1s <elitnev@gmail.com>
2025-01-14 00:24:06 +0900

    Update videobg.js

--- js/videobg.js
+++ js/videobg.js
@@ -1,8 +1,8 @@
-const vNames = ["Tqlw87XV4kU", "GQEcxrY0CWA", "3lDqMx4rmFU", "YudHcBIxlYw", "b73BI9eUkjM", "hOgVAYpHPCc"];
+const vNames = ["65514274", "35706887", "809928207", "68722687", "846661461", "443455797"];
 const vParam = vNames[parseInt(Math.random() * vNames.length)];
 
 const vDiv = document.querySelector("#video div");
 
-vDiv.setAttribute("data-vbg", `https://www.youtube.com/watch?v=${vParam}`);
+vDiv.setAttribute("data-vbg", `https://vimeo.com/${vParam}`);
 
 const videoBackgrounds = new VideoBackgrounds('[data-vbg]');
